207f001e4b7c54f4f117138e8319f3cdd24241e4
michael carini <snownkc@icloud.com>
2021-08-03 20:28:43 +0100

    started classesLAB

---
+++ ClassesStructsLABSol/ClassesStructsLAB/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesStructsLAB;
+
+namespace ClassesStructsLAB
+{
+    public class Vehicle
+    {
+        private int _capacity;
+        private int _numPassengers;
+
+
+
+        public int Position { get; private set; }
+        public int Speed { get; init; }        
+        public int NumPassengers { get; set; }
+
+
+        public Vehicle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public string Move()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
